10cefa875db578e29a3c136f0e4616c26e5c2b8e
Lotas-Dan <eydins281@gmail.com>
2022-06-02 03:33:56 +0300

    Fix ban & kick CMD

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,12 +3,12 @@ const db = require('quick.db');
 const { errLogChannelID } = require('../../config.json');
 
 module.exports = {
-        name: "ban",
-        aliases: ["b", "banish"],
-        category: "moderation",
-        description: "Bans the user",
-        usage: "[name | nickname | mention | ID] <reason> (optional)",
-        accessableby: "Administrator",
+    name: "ban",
+    aliases: ["b", "banish"],
+    category: "moderation",
+    description: "Bans the user",
+    usage: "[name | nickname | mention | ID] <reason> (optional)",
+    accessableby: "Administrator",
     run: async (bot, message, args) => {
         try {
             if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**You Dont Have The Permissions To Ban Users! - [BAN_MEMBERS]**");
@@ -21,30 +21,26 @@ module.exports = {
 
             var reason = args.slice(1).join(" ");
 
-            if (!banMember.bannable) return message.channel.send("**Cant Kick That User**")
-            try {
+            if (!banMember.bannable) return message.channel.send(`**Cannot Ban ${banMember} Maybe His Role Higher Than My**`)
+
             banMember.send(`**Hello, You Have Been Banned From ${message.guild.name} for - ${reason || "No Reason"}**`).then(() =>
-                message.guild.members.ban(banMember, { days: 7, reason: reason })).catch(() => null)
-            } catch {
-                message.guild.members.ban(banMember, { days: 7, reason: reason })
-            }
+                message.guild.members.ban(banMember, { days: 7, reason: reason })).catch(() => {
+                    message.guild.members.ban(banMember, { days: 7, reason: reason })
+                })
+
             if (reason) {
-            var sembed = new MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${banMember.user.username}** has been banned for ${reason}`)
-            message.channel.send(sembed)
+                var sembed = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setAuthor(message.guild.name, message.guild.iconURL())
+                    .setDescription(`**${banMember.user.username}** has been banned for ${reason}`)
+                message.channel.send(sembed)
             } else {
                 var sembed2 = new MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${banMember.user.username}** has been banned`)
-            message.channel.send(sembed2)
+                    .setColor("GREEN")
+                    .setAuthor(message.guild.name, message.guild.iconURL())
+                    .setDescription(`**${banMember.user.username}** has been banned`)
+                message.channel.send(sembed2)
             }
-            let channel = db.fetch(`modlog_${message.guild.id}`)
-            if (channel == null) return;
-
-            if (!channel) return;
 
             const embed = new MessageEmbed()
                 .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
@@ -59,8 +55,8 @@ module.exports = {
                 .addField("**Date**", message.createdAt.toLocaleString())
                 .setTimestamp();
 
-                if(!errLogChannelID){ var sChannel = message.guild.channels.cache.get(channel)}
-                else{var sChannel = message.guild.channels.cache.get(errLogChannelID)}
+            var sChannel = message.guild.channels.cache.get(errLogChannelID)
+            if (!errLogChannelID) return;
             if (!sChannel) return;
             sChannel.send(embed)
         } catch (e) {
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,13 +1,14 @@
 const { MessageEmbed } = require("discord.js");
 const db = require('quick.db');
+const { errLogChannelID } = require('../../config.json');
 
 module.exports = {
-        name: "kick",
-        aliases: ["k"],
-        category: "moderation",
-        description: "Kicks the user",
-        accessableby: "Administrator",
-        usage: "[name | nickname | mention | ID] <reason> (optional)",
+    name: "kick",
+    aliases: ["k"],
+    category: "moderation",
+    description: "Kicks the user",
+    accessableby: "Administrator",
+    usage: "[name | nickname | mention | ID] <reason> (optional)",
     run: async (bot, message, args) => {
         try {
             if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("**You Do Not Have Permissions To Kick Members! - [KICK_MEMBERS]**");
@@ -20,34 +21,30 @@ module.exports = {
 
             if (kickMember.id === message.member.id) return message.channel.send("**You Cannot Kick Yourself!**")
 
-            if (!kickMember.kickable) return message.channel.send("**Cannot Kick This User!**")
+            if (!kickMember.kickable) return message.channel.send(`**Cannot Kick ${kickMember} Maybe His Role Higher Than My**`)
 
             var reason = args.slice(1).join(" ");
-            try {
-                const sembed2 = new MessageEmbed()
-                    .setColor("RED")
-                    .setDescription(`**Hello, You Have Been Kicked From ${message.guild.name} for - ${reason || "No Reason!"}**`)
-                    .setFooter(message.guild.name, message.guild.iconURL())
-                kickMember.send(sembed2).then(() =>
-                    kickMember.kick()).catch(() => null)
-            } catch {
-                kickMember.kick()
-            }
+
+            const sembed2 = new MessageEmbed()
+                .setColor("RED")
+                .setDescription(`**Hello, You Have Been Kicked From ${message.guild.name} for - ${reason || "No Reason!"}**`)
+                .setFooter(message.guild.name, message.guild.iconURL())
+            kickMember.send(sembed2).then(() =>
+                kickMember.kick()).catch(() => kickMember.kick())
+
             if (reason) {
-            var sembed = new MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${kickMember.user.username}** has been kicked for ${reason}`)
-            message.channel.send(sembed);
+                var sembed = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setAuthor(message.guild.name, message.guild.iconURL())
+                    .setDescription(`**${kickMember.user.username}** has been kicked for ${reason}`)
+                message.channel.send(sembed);
             } else {
-                var sembed2 = new MessageEmbed()
-                .setColor("GREEN")
-                .setAuthor(message.guild.name, message.guild.iconURL())
-                .setDescription(`**${kickMember.user.username}** has been kicked`)
-            message.channel.send(sembed2);
+                var sembed3 = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setAuthor(message.guild.name, message.guild.iconURL())
+                    .setDescription(`**${kickMember.user.username}** has been kicked`)
+                message.channel.send(sembed3);
             }
-            let channel = db.fetch(`modlog_${message.guild.id}`)
-            if (!channel) return;
 
             const embed = new MessageEmbed()
                 .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
@@ -61,7 +58,8 @@ module.exports = {
                 .addField("**Date**", message.createdAt.toLocaleString())
                 .setTimestamp();
 
-            var sChannel = message.guild.channels.cache.get(channel)
+            var sChannel = message.guild.channels.cache.get(errLogChannelID)
+            if (!errLogChannelID) return;
             if (!sChannel) return;
             sChannel.send(embed)
         } catch (e) {
